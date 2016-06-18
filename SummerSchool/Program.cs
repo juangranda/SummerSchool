27c91fe2047ef265b5dabd1afc7e645c04e7dac0
Juan Granda <juanfromecuador@gmail.com>
2016-06-17 20:37:16 -0400

    menu and option 1 methods

--- SummerSchool/Program.cs
+++ SummerSchool/Program.cs
@@ -10,14 +10,49 @@ namespace SummerSchool
     {
         static void Main(string[] args)
         {
+
+            Menu();
+
+            string choice = Console.ReadLine();
+            MenuChoice(choice);
+            if (Is1(choice) == true)
+            {
+                Option1();
+            }
+
+               
+            Console.ReadKey();
+        }
+        public static void Menu()
+        {
             Console.WriteLine("1) Enroll a student");
             Console.WriteLine("2) Unenroll a student");
             Console.WriteLine("3) Print out the list of enrolled students");
             Console.WriteLine("4) Exit");
-
-        Console.ReadKey();
         }
 
-        
+        public static string MenuChoice(string choice)
+        {
+            return choice;
+        }
+
+        public static bool Is1(string choice)
+        {
+            if (choice == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public static void Option1()
+        {
+            Console.WriteLine("Please enter the name of the student you want to enroll");
+        }
+
     }
 }
